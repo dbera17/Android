d3782633d482d06eccb28a8b636e1f08edeb618a
dbera17 <dbera17@freeuni.edu.ge>
2021-08-07 00:08:34 +0400

    need firebase to register

--- app/src/main/java/ge/dbera17/finalproject/Registration.kt
+++ app/src/main/java/ge/dbera17/finalproject/Registration.kt
@@ -3,9 +3,39 @@ package ge.dbera17.finalproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
 class Registration : AppCompatActivity() {
+    private lateinit var nickname: EditText
+    private lateinit var password: EditText
+    private lateinit var profession: EditText
+
+    private lateinit var signUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registration)
+        setUpItems()
+    }
+
+    private fun setUpItems() {
+        signUp = findViewById(R.id.rp_sign_up)
+        nickname = findViewById(R.id.nickname)
+        password = findViewById(R.id.password)
+        profession = findViewById(R.id.profession)
+
+        signUp.setOnClickListener {
+            signUpNewUser()
+        }
+    }
+
+    private fun signUpNewUser() {
+        if(nickname.text.isNotEmpty() && password.text.isNotEmpty() && profession.text.isNotEmpty()){
+            Toast.makeText(this, "Create User", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_LONG).show()
+        }
     }
 }
