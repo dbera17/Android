d41bf4f6e129864bd9ec43203fd1df18c67b671b
dbera17 <dbera17@freeuni.edu.ge>
2021-08-06 23:29:32 +0400

    class files started and go to registration added

--- app/src/main/java/ge/dbera17/finalproject/MainActivity.kt
+++ app/src/main/java/ge/dbera17/finalproject/MainActivity.kt
@@ -1,11 +1,25 @@
 package ge.dbera17.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var signUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        signUp = findViewById(R.id.sign_up)
+        signUp.setOnClickListener {
+            goToRegistration()
+        }
+    }
+
+    private fun goToRegistration() {
+        val registrationPageInt = Intent(this, Registration::class.java)
+        startActivity(registrationPageInt)
     }
 }
---
+++ app/src/main/java/ge/dbera17/finalproject/Registration.kt
@@ -0,0 +1,11 @@
+package ge.dbera17.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registration)
+    }
+}
